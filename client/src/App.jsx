3fea9927fd090b24fad63ffe6e3619344718f25c
Iebs15 <ayush.kumar@iebrain.com>
2025-08-25 13:51:50 +0530

    app.jsx fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -66,58 +66,6 @@ const AppContent = () => {
   )
 }
 
-// Create a wrapper component to use hooks inside Router
-const AppContent = () => {
-  // This function will be called by useAuthUrl when a reset password link is clicked
-  const handleOpenAuthModal = (mode = 'login', resetToken = null) => {
-    // Dispatch a custom event that the Header can listen to
-    window.dispatchEvent(new CustomEvent('openAuthModal', { 
-      detail: { mode, resetToken } 
-    }))
-  }
-
-  // Use the auth URL hook to handle password reset links
-  useAuthUrl(handleOpenAuthModal)
-
-  return (
-    <div className="min-h-screen transition-all duration-500 flex flex-col">
-      <Routes>
-        {/* Landing page route */}
-        <Route 
-          path="/" 
-          element={
-            <>
-              <Header />
-              <main className="flex-1">
-                <MainContent />
-              </main>
-            </>
-          } 
-        />
-        
-        {/* Chat session route */}
-        <Route 
-          path="/chat/:sessionId" 
-          element={<ChatSession />} 
-        />
-        
-        {/* Fallback to landing page */}
-        <Route 
-          path="*" 
-          element={
-            <>
-              <Header />
-              <main className="flex-1">
-                <MainContent />
-              </main>
-            </>
-          } 
-        />
-      </Routes>
-    </div>
-  )
-}
-
 const App = () => {
   return (
     <ThemeProvider>
